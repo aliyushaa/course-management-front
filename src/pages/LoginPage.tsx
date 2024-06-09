915955f6671183cad8d2c02131f889ab85f70194
aliyushaa <97094050+aliyushaa@users.noreply.github.com>
2024-06-09 16:12:06 +0500

    login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,8 @@
 import React, {useContext} from 'react'
+import './LoginForm.css'
+import { FaUserAlt } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
 import {SubmitHandler, useForm} from "react-hook-form";
 import {IAuthForm} from "../types/auth.types";
 import {authService} from "../services/auth.service";
@@ -43,58 +47,61 @@ export default observer(function LoginPage() {
 
     return (
         <>
-            <ToastContainer
-                position="top-right"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-            />
-            <div style={{ backgroundColor: '#add8e6', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <div style={{ width: '90%', maxWidth: '400px', backgroundColor: 'white', padding: '20px', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
-                    <h4 className="text-xl text-center pb-5 font-bold text-black">Login</h4>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <div className="mb-6">
-                            <label htmlFor="email"
-                                   className="block mb-2 text-sm font-medium text-black">Email address</label>
-                            <input id="email"
-                                   type="email"
-                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                   placeholder="email@gmail.com"
-                                   {...register('email', {
-                                       required: 'Email must be provided'
-                                   })}
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <label htmlFor="password"
-                                   className="block mb-2 text-sm font-medium text-black">Password</label>
-                            <input type="password"
-                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                   placeholder="•••••••••"
-                                   {...register('password', {
-                                       required: 'Password must be provided'
-                                   })}
-                            />
-                        </div>
-                        <p className="text-red-600">{errors.email?.message}</p>
-                        <p className="text-red-600">{errors.password?.message}</p>
-                        <div className="text-end items-end mb-6">
-                            <label htmlFor="remember"
-                                   className="ml-2 text-sm text-end font-medium text-gray-900">
-                                <a href="#" className="text-black hover:underline">Forget Password ?</a></label>
-                        </div>
-                        <button type="submit"
-                                className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                            Login
-                        </button>
-                    </form>
-                </div>
+            <div className="LoginPage">
+            <div className='wrapper'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <h1>
+                        Login
+                    </h1>
+                    <div className="input-box">
+                        {/*<input type="text" placeholder='Username' required/>*/}
+                        <input id="email"
+                               type="email"
+                               placeholder="email@gmail.com"
+                               {...register('email', {
+                                   required: 'Email must be provided'
+                               })}
+                        />
+
+                        <FaUserAlt className='icon'/>
+
+                    </div>
+                    <div className="input-box">
+                        <input type="password"
+                               placeholder="Password"
+                               {...register('password', {
+                                   required: 'Password must be provided'
+                               })}
+                        />
+                        <FaLock className='icon'/>
+
+                    </div>
+
+                    <div className="remember-forgot">
+                        <label><input type="checkbox"/> Remember me </label>
+                    </div>
+
+                    <button type="submit">
+                        Login
+                    </button>
+
+
+                </form>
+            </div>
+
+                <ToastContainer
+                    position="top-right"
+                    autoClose={3000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                />
+
             </div>
         </>
     )
